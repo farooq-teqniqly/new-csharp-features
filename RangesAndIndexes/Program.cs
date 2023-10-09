345c8fcb40cc95807cd8ef1c0067c773ac723f81
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2023-10-09 09:31:02 -0400

    Ranges and indexes

---
+++ RangesAndIndexes/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+var sequence = Sequence(1000);
+
+for (var start = 0; start < sequence.Length; start += 100)
+{
+    var r = start..(start + 10);
+    var (min, max, average) = MovingAverage(sequence, r);
+    Console.WriteLine($"From {r.Start} to {r.End}:    \tMin: {min},\tMax: {max},\tAverage: {average}");
+}
+
+for (var start = 0; start < sequence.Length; start += 100)
+{
+    var r = ^(start + 10)..^start;
+    var (min, max, average) = MovingAverage(sequence, r);
+    Console.WriteLine($"From {r.Start} to {r.End}:  \tMin: {min},\tMax: {max},\tAverage: {average}");
+}
+
+(int min, int max, double average) MovingAverage(int[] subSequence, Range range) =>
+(
+    subSequence[range].Min(),
+    subSequence[range].Max(),
+    subSequence[range].Average()
+);
+
+int[] Sequence(int count) =>
+    Enumerable.Range(0, count).Select(x => (int)(Math.Sqrt(x) * 100)).ToArray();
